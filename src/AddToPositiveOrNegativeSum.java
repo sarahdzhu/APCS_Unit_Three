9a4b1f6a2a48cadb42f1dcbb33d400c320fedd24
sarahdzhu <zhudo@ssfs.org>
2021-10-20 11:10:05 -0400

    3.4 Practice

--- src/AddToPositiveOrNegativeSum.java
+++ src/AddToPositiveOrNegativeSum.java
@@ -1,17 +1,40 @@
 import java.util.Scanner;
 public class AddToPositiveOrNegativeSum {
     public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
         // declare and instantiate a Scanner variable and object
+       int positiveSum=0;
+       int negativeSum=0;
+
+
 
         // declare and initialize variables for a positive and negative sum
 
         for (int i = 1; i <= 10; i++) {
             /* in this block, allow the user to input a number and add the number to the appropriate sum */
+            System.out.print("Enter a number: ");
+            int num= scan.nextInt();
+
+        if (num>0) {
+            positiveSum+=num;
+        }
+        if (num<0){
+            negativeSum+=num;
+        }
+            System.out.println("The sum of the positive numbers is "+ positiveSum);
+            System.out.println("The sum of the negative numbers is "+ negativeSum);
+
+
+
+
 
 
         } // end of for loop
 
-        // output the sums. Make sure the format matches the sample
+
+
+
+            // output the sums. Make sure the format matches the sample
 
     }
 }
--- src/FindLargest.java
+++ src/FindLargest.java
@@ -5,6 +5,31 @@ public class FindLargest {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int first = scan.nextInt();
+        System.out.print("Enter the second number: ");
+        int second = scan.nextInt();
+    if (first>second){
+        System.out.println(first+" is larger than "+second);
+    }
+        if(second>first){
+            System.out.println(second+" is larger than "+first);
+        }
+        if(second==first){
+            System.out.println(first+" and "+second+" are equal");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
         // Prompt the user for two integer variables
 
