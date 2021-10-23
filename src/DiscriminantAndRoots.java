487fd224340cf567d13fcef7842318523bb592c5
sarahdzhu <zhudo@ssfs.org>
2021-10-22 23:02:16 -0400

    Roots

--- src/DiscriminantAndRoots.java
+++ src/DiscriminantAndRoots.java
@@ -2,8 +2,20 @@ public class DiscriminantAndRoots {
 
     // Create the method roots in the space below.
     public static String roots(int a, int b, int c) {
+        int d = (int) (Math.pow(b, 2)) - (4 * a * c);
+        String result=a+"x^2 "+"+ "+b+"x + "+c;
+        if (d == 0) {
+            return result+" has 1 real root";
+        } else if (d < 0) {
+            return result+" has 2 imaginary roots";
+        } else
+            return result+" has 2 real roots";
 
-        return ""; // update this line so that it returns properly formatted output
     }
-
 }
+
+        // update this line so that it returns properly formatted output
+
+
+
+
