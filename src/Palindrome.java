b31b60ab53262a23452b591d10542fabca0dc746
sarahdzhu <zhudo@ssfs.org>
2021-10-25 16:04:19 -0400

    3.9 Practice

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,6 +1,30 @@
 public class Palindrome {
+    public static boolean isPalindrome(int a) {
+        boolean palindromeYes = true;
+        boolean palindromeNo = false;
+        int b = a % 10;
+        int c = (a % 100)/10;
+        int d = a / 10000;
+        int e = (a % 10000)/1000;
+        if (a <= 99999 && a>9999) {
+            if (b == d) {
+                if (c == e) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
 
-    // Write your isPalindrome method in the space below. You do not need to write a main method for this program
-    // Your method should be static.
 
-}
+
+
+    }}
+
