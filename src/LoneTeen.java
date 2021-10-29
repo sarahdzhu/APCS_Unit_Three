206c3b6a8540d0c6b487ea1a871785d0b9c35b21
sarahdzhu <zhudo@ssfs.org>
2021-10-28 21:18:16 -0400

    Lone Teen

--- src/LoneTeen.java
+++ src/LoneTeen.java
@@ -1,5 +1,21 @@
 public class LoneTeen {
+    public static boolean loneTeen(int num1, int num2) {
+        if (num1>=13 && num1<=19) {
+            if (num2 <= 19 && num1 >= 13) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        else if(num2 <=19 && num2>=13){
+            return true;
+        }
+        else{
+            return false;
 
-    // In the space below, write the loneTeen method. Make sure the signature matches
-    // the one given in the instructions.
-}
+        }}}
+
+
+        // In the space below, write the loneTeen method. Make sure the signature matches
+        // the one given in the instructions.
+
